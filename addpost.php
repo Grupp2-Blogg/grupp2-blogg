88ab02c33507dff40a9fb30d91e5522fd215a513
Jennifer MacFie <jenmacfie@hotmail.com>
2025-02-27 10:42:11 +0100

    Added code so you can upload an image with the post

--- addpost.php
+++ addpost.php
@@ -13,6 +13,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $content = $_POST['blogcontent'];
     $user_id = $_SESSION['user_id'];
 
+    $imagePath = NULL;
+    if (isset($_FILES['post_image'])&& $_FILES['post_image']['error'] === UPLOAD_ERR_OK){
+        $uploadDir = "uploads/";
+        $fileName = basename($_FILES['post_image']['name']);
+        $targetFilePath = $uploadDir . time() . "_" . $fileName;
+
+        if (move_uploaded_file($_FILES['post_image']['tmp_name'], $targetFilePath)){
+            $imagePath = $targetFilePath;
+        }
+        else{
+            echo "Error: Couldn't upload the image!";
+        }
+    }
+
 
     try{
         $stmt = $pdo->prepare("INSERT INTO blogposts (blogtitle, blogcontent, user_id) VALUES (?,?,?)");
@@ -29,8 +43,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 <h2>Add a new post to your blog</h2>
-<form method="POST">
+<form method="POST" enctype="multipart/form-data">
     <input type="text" name="blogtitle" placeholder="Post Title" required>
     <textarea name="blogcontent" placeholder="Write here..." required></textarea>
+    <input type = "file" name="post_image" accept="image/*">
     <button type="submit">Publish Post</button>
 </form>
